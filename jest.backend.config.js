85cf79fe05fe42dc64e55bc210e114f8335d651b
lws49 <e0970244@u.nus.edu>
2026-02-17 21:16:14 +0800

    update jest config files to exclude _site and runs all frontend and backend tests

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -5,12 +5,19 @@ export default {
   // when testing backend
   testEnvironment: "node",
 
-  // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  // which test to run - all backend tests, excluding client and node_modules
+  testMatch: ["<rootDir>/**/*.test.js"],
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/client/"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/**"],
+  collectCoverageFrom: [
+    "controllers/**",
+    "models/**",
+    "middlewares/**",
+    "helpers/**",
+    "config/**",
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
