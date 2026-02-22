388cffa4fb202e9226ad6436b8118ac2d64e477f
Priyansh B <priyanshmagic@gmail.com>
2026-02-23 05:48:37 +0800

    update jest config

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,21 +6,21 @@ export default {
   testEnvironment: "node",
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js",
-    "<rootDir>/models/*.test.js"
-  ],
-
-
   testMatch: [
-    "<rootDir>/controllers/*.test.js",
-    "<rootDir>/helpers/*.test.js",
-    "<rootDir>/middlewares/*.test.js",
+    "<rootDir>/helpers/authHelper.test.js",
+    "<rootDir>/middlewares/authMiddleware.test.js",
+    "<rootDir>/controllers/authController.test.js",
+    "<rootDir>/controllers/categoryController.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/**", "models/**"],
-  collectCoverageFrom: ["controllers/**", "helpers/**", "middlewares/**"],
+  collectCoverageFrom: [
+    "helpers/authHelper.js",
+    "middlewares/authMiddleware.js",
+    "controllers/authController.js",
+    "controllers/categoryController.js",
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
